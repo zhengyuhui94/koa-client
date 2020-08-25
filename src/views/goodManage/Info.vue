1eeda5969c54bd205b3832516042ff00eb91f8db
zhengyuhui94 <zhengyuhui94@126.com>
2020-08-25 15:37:32 +0800

    修改为https 且对每个接口服务做代理

--- src/views/goodManage/Info.vue
+++ src/views/goodManage/Info.vue
@@ -144,7 +144,7 @@
 
         // 导出数据
         private async exportExcel(): Promise<void>{
-            let path = 'http://koamall.top:8081/goodManage/info/exportExcel';
+            let path = 'https://koamall.top/api/goodManage/info/exportExcel';
             if(process.env.NODE_ENV === 'development'){
                 path = 'http://localhost:8081/goodManage/info/exportExcel';
             }
